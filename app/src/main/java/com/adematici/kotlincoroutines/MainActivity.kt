dd061e74edd9e2dbceb959375fd54c8c64f40892
Adem Atici <adematicis41@gmail.com>
2021-02-19 19:03:19 +0300

    First commit

---
+++ app/src/main/java/com/adematici/kotlincoroutines/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.adematici.kotlincoroutines
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
