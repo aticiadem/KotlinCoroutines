beceb3ba7604fa5ce45ca064d91b302825f0bab5
Adem Atici <adematicis41@gmail.com>
2021-02-21 10:48:20 +0300

    GlobalScope ve runBlocking

--- app/src/main/java/com/adematici/kotlincoroutines/MainActivity.kt
+++ app/src/main/java/com/adematici/kotlincoroutines/MainActivity.kt
@@ -2,14 +2,32 @@ package com.adematici.kotlincoroutines
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var i: Int = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        println("runBlocking basladi.")
+        runBlocking {
+            launch {
+                delay(4000)
+                println("runBlocking")
+            }
+        }
+        println("runBlocking bitti.")
 
+        println("GlobalScope basladi.")
+        GlobalScope.launch {
+            delay(4000)
+            println("GlobalScope")
+        }
+        println("GlobalScope bitti.")
+        // runBlocking main threadi blokladıgı icin arkaplanda calismaz ve islemin bitmesi gerekir.
+        // GlobalScope ise arkaplanda calismaya devam eder, main threadi bloklamaz.
     }
 }
